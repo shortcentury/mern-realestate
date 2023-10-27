aec09384058196ca2ccc7b816df9fc72fea53893
shortcentury <tsaridakis@gmail.com>
2023-10-27 23:57:13 +0300

    Connect to MongoDB

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to={"/"}>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="">Apidi</span>
+            <span className="">Estate</span>
+          </h1>
+        </Link>
+        <form className="p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            placeholder="Search..."
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to={"/"}>
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to={"/about"}>
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to={"/signin"}>
+            <li className="inline text-slate-700 hover:underline cursor-pointer">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
